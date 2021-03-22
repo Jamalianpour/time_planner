48d82f8eea9bf05d660a1df395906a006d2a01c4
Mohammad Jamalianpour <jamalianm21@yahoo.com>
2021-03-22 20:53:30 +0430

    remove test, add homepage url

--- test/time_planner_test.dart
+++ test/time_planner_test.dart
@@ -1,7 +1,7 @@
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:time_planner/time_planner.dart';
+// import 'package:time_planner/time_planner.dart';
 
-void main() {
+// void main() {
 
-}
+// }
